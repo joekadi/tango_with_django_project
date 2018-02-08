68aee607be0578eef2c8f5a599cfbdd991533fa0
Joe Kadi <2261087k@student.gla.ac.uk>
2018-02-08 11:53:18 +0000

    Chapter 10

--- rango/views.py
+++ rango/views.py
@@ -9,18 +9,60 @@ from django.contrib.auth import authenticate, login
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
 
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
 
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
+    
 def index(request):
+    request.session.set_test_cookie()
+    
     category_list = Category.objects.order_by('-likes')[:5]
     pages_list = Page.objects.order_by('-views')[:5]
+    
     context_dict = {'categories': category_list, 'page': pages_list}
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+    
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/index.html', context=context_dict)
+
+    return response
 
 def about(request):
-    about_context_dict = {'boldmessage': "Rango says here is the about page."}
-    return render(request, 'rango/about.html', about_context_dict)
+    context_dict = {'boldmessage': "Rango says here is the about page."}
+    
+    if request.session.get('visits'):
+        visits = request.session.get('visits')
+    else:
+        visits = 0
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    
+    # if request.session.test_cookie_worked():
+      #  print("TEST COOKIE WORKED!")
+       # request.session.delete_test_cookie()
+        
+    response = render(request, 'rango/about.html', context_dict)
+    return response
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -121,15 +163,19 @@ def user_login(request):
             else:
                 return HttpResponse("Your Rango account is disabled.")
         else:
-            print("Invalid login details: {0}, {1}".format(username, password))
-            return HttpResponse("Invalid login details supplied.")
-
+            if not username and not password:
+                print("Invalid login details: {0}, {1}".format(username, password))
+                return HttpResponse("Invalid login details supplied.")
+            elif not password:
+                return HttpResponse("Password is incorrect")
+            else:
+                return HttpResponse("No account exists with that username")
     else:
         return render(request, 'rango/login.html', {})
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {})
 
 @login_required
 def user_logout(request):
